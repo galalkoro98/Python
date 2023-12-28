7cfd422172fd337be192223156378f176631d252
galalkoro98 <galal.koro@gmail.com>
2023-12-28 19:53:06 +0100

    add new file

---
+++ loop.py
@@ -0,0 +1,19 @@
+# print("The break instruction:")
+
+# for i in range(1, 6):
+#     if i == 3:
+#         break
+#     print("inside the loop", i)
+# print("outside of the loop")
+
+# for i in range(1, 6):
+#     if i == 3:
+#         continue
+#     print("skip this iteration", i)
+#     print("outside of the loop")
+number = int(input("Entre number: "))
+
+for number in range(5):
+        print(number)
+    
+    
