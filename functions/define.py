e3b40b198932e5f1242dfceeadefcd1949c53661
galalkoro98 <galal.koro@gmail.com>
2024-01-07 08:51:17 +0100

    add new files about define function in python

---
+++ functions/define.py
@@ -0,0 +1,24 @@
+# list = [4, 10, 14, 8, 40, 37]
+
+# def my_list():
+#    for i in list:
+#       if i % 2 == 0:
+#          print(i)
+# my_list()
+
+
+# number = int(input("Entre a number: "))
+
+# def message(number):
+#     if number > 0:
+#         return f"The number {number} is positive."
+#     elif number < 0:
+#         return f"The number {number} is negative."
+#     else:
+#         return f"The number {number} is zero."
+# print(message(number))
+
+def message(number):
+    print("Enter a number:", number)
+
+print(message(1))
---
+++ functions/parameter.py
@@ -0,0 +1,10 @@
+first_name = input("Entre Your First Name: ")
+last_name = input("Entre your last name: ")
+
+
+def introduction(first_name, last_name):
+    print('Hello, my name is', first_name, last_name)
+    
+
+introduction(first_name, last_name)
+
