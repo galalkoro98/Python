1ca7ded299e9dca2943061369aaaf496423d1152
galalkoro98 <galal.koro@gmail.com>
2024-01-09 09:01:53 +0100

    update file

---
+++ functions/effect_and_result.py
@@ -0,0 +1,28 @@
+# def happy_new_year(wishes = True):
+#     print("Three ...")
+#     print("Two...")
+#     print("One...")
+#     if not wishes:
+#         return
+#     print("Happy New Year!")
+
+# happy_new_year(False)
+
+
+# def list_sum(lst):
+#     s = 0
+
+#     for elem in lst:
+#         s = s + elem
+    
+#     return s
+
+# print(list_sum([10]))
+
+
+def create_list(n):
+    my_list = []
+    for i in range(n):
+        my_list.append(i)
+    return my_list
+
